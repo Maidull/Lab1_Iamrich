e647f7a89217c22d1be5ccc05f7f4db21d7b604f
Mai <mainguyen2913@gmail.com>
2024-12-08 18:07:42 +0700

    update lab 1

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+     MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text("I Am Rich",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.lightGreenAccent,
+            ),
+          ),
+          backgroundColor: Colors.white,
+          elevation: 0,
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('assets/images/rich.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+
